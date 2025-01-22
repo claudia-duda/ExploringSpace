20eb79ba5a10870ec39ac17abc6f82e86c457035
Claudia Ramos <claudiaeduarda_duda@hotmai.com>
2025-01-22 12:36:40 -0300

    Creating forms  + views to authenticate users

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,63 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from usuarios.forms import CadastroForms, LoginForms
+from django.contrib import auth
+from django.contrib.auth.models import User
+
 
 def login (request):
-    return render(request, "usuarios/login.html")
+    form = LoginForms()
+
+    if request.method == 'POST':
+        form = LoginForms(request.POST)
+
+        if form.is_valid():
+
+            nome = form["nome_login"].value()
+            senha = form["senha"].value()
+
+            usuario = auth.authenticate(
+                request,
+                username = nome,
+                password = senha
+            )
+
+            if usuario is not None:
+                auth.login(request, usuario)
+                messages.success(request, "Bem vindo(a)")
+                return redirect('index')
+            else:
+                messages.error(request, "Erro ao realizar ao login")
+                return redirect('login')
+
+    return render(request, "usuarios/login.html", {"form" : form })
 
 def cadastro(request):
-   return render(request, "usuarios/cadastro.html")
+    form = CadastroForms()
+
+    if request.method == 'POST':
+        form = CadastroForms(request.POST)
+
+        if form.is_valid():
+
+            nome = form["nome_cadastro"].value()
+            email = form["email"].value()
+            senha = form["senha_inicial"].value()
+
+            if User.objects.filter(username = nome).exists():
+                return redirect('cadastro')
+
+            if senha != form["senha_confirmar"].value():
+                messages.error(request, "Senhas não são iguais")
+                return redirect('cadastro')
+
+            usuario = User.objects.create_user(
+                username = nome,
+                email = email,
+                password = senha
+            )
+            usuario.save()
+            messages.success(request, f"Usuario {nome} criado com sucesso!")
+            return redirect('login')
+
+    return render(request, "usuarios/cadastro.html", { "form" : form })
