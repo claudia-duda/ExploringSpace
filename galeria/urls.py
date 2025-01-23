8141b87179f6b88648fa19f972acd9894706a774
Claudia Ramos <claudiaeduarda_duda@hotmai.com>
2025-01-23 15:05:25 -0300

    Custom home to unauthenticated users

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from galeria.views import index, imagem, buscar
+from galeria.views import index, imagem
 
 urlpatterns = [
     path('', index, name='index'),
     path('imagem/<int:foto_id>', imagem, name='imagem'),
-    path('buscar', buscar, name='buscar'),
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -1,23 +1,13 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from galeria.models import Fotografia
 from django.contrib import messages
-def index(request):
-    if not request.user.is_authenticated:
-        messages.error(request, "Usuário não logado")
-        return redirect('login')
-
-    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
-    return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
 
-def buscar(request):
-    if not request.user.is_authenticated:
-        messages.error(request, "Usuário não logado")
-        return redirect('login')
-
+def index(request):
+    categorias  = Fotografia.OPCOES_CATEGORIA
     fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
 
     if "buscar" in request.GET:
@@ -25,4 +15,9 @@ def buscar(request):
         if nome_a_buscar:
             fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
 
-    return render(request, "buscar.html", {"cards": fotografias})
+    if "categoria" in request.GET:
+        categoria = request.GET["categoria"]
+        if categoria:
+            fotografias = fotografias.filter(categoria=categoria)
+
+    return render(request, "galeria/index.html", {"cards": fotografias, "categorias" : categorias})
